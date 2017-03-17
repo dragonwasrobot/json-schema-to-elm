032f38e0601c1fb2b3bd5b1d0d62ac7ee4bc7632
Peter Urbak <pu@opentelehealth.com>
2017-03-17 23:43:07 +0100

    Adds ExDoc

--- mix.exs
+++ mix.exs
@@ -1,13 +1,16 @@
-defmodule ElmGenerator.Mixfile do
+defmodule JS2.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :decoder,
-     version: "1.0.0",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [app: :js2e,
+     version: "0.1.0",
+     elixir: "~> 1.4",
+     deps: deps(),
+
+     # Docs
+     name: "JS2E",
+     source_url: "https://github.com/dragonwasrobot/json-schema-to-elm/"
+    ]
   end
 
   def application do
@@ -16,6 +19,7 @@ defmodule ElmGenerator.Mixfile do
 
   defp deps do
     [{:poison, "~> 3.0"},
+     {:ex_doc, "~> 0.14", only: :dev, runtime: false},
      {:credo, "~> 0.5", only: [:dev, :test]},
      {:dialyxir, "~> 0.4", only: [:dev], runtime: false},
      {:apex, "~>1.0.0"}
