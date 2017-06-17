f97f5430b2e5ad5a1e2e1bbd3c5a47b37ac61167
Peter Urbak <pu@opentelehealth.com>
2017-06-18 01:33:25 +0200

    #23: Adds doctests for TypePath

--- test/parser_test.exs
+++
@@ -1,4 +0,0 @@
-defmodule JS2ETest.Parser do
-  use ExUnit.Case, async: true
-  doctest JS2E.Parser
-end
---
+++ test/type_path_test.exs
@@ -0,0 +1,4 @@
+defmodule JS2ETest.TypePath do
+  use ExUnit.Case
+  doctest JS2E.TypePath
+end
