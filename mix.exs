6276ce8bcc71f030675891ce2319181ef04ceb57
Peter Urbak <peter@dragonwasrobot.com>
2018-07-10 00:05:33 +0200

    Updates version in mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule JS2E.Mixfile do
   def project do
     [
       app: :js2e,
-      version: "2.0.0",
+      version: "2.6.0",
       elixir: "~> 1.6",
       deps: deps(),
       aliases: aliases(),
